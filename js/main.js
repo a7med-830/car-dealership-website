2a4c9583926285c39b34c0eb3373d1bf889c036b
a7med-830 <830ahmedelsheikh@gmail.com>
2026-02-28 17:14:21 +0200

    initial project structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// General logic (Mobile menu, etc.)
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website loaded successfully.");
+
+    // Add mobile menu toggle logic here
+});
